8c23c7c47face14463882e4b13262ecd5da7b87e
fatematujjohra <fatematujjohralima2@gmail.com>
2026-03-14 20:37:04 +0600

    keys values & entries

---
+++ object.js
@@ -0,0 +1,6 @@
+const king = { name: 'Mufasa', age: 55, kingdom: 'Pride Lands' };
+const keys = Object.keys(king);
+const values = Object.values(king)
+const entries = Object.entries(king);
+console.log(entries);
+
