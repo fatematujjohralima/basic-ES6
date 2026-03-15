afe342bb0c7e2fd61d3125895ba2cef88ad2d265
fatematujjohra <fatematujjohralima2@gmail.com>
2026-03-15 15:20:06 +0600

    object(keys values entries freeze seal)

--- object.js
+++ object.js
@@ -4,3 +4,13 @@ const values = Object.values(king)
 const entries = Object.entries(king);
 console.log(entries);
 
+
+// Object.freeze(king);   value push update konotai kora jabe na, delete o kora jabe na, add o kora jabe na
+// Object.seal(king);   value update kora jabe kintu push kora jabe na
+
+Object.seal(king);
+delete king.age;
+delete king.kingdom;
+king.queen = 'Sarabi';
+king.name = 'King Simba';
+console.log(king);
