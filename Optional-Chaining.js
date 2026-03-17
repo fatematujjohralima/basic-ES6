8b60f4fca391beb4030f18c29ed5038dd01d13c6
fatematujjohra <fatematujjohralima2@gmail.com>
2026-03-17 23:53:59 +0600

    added optional chaining (?) for safe property access

---
+++ Optional-Chaining.js
@@ -0,0 +1,27 @@
+const person = {
+    name: "hena",
+    1: "100",
+    true:"sdfdsf",
+    age: 30,
+    friends: ["korim", "hero alom"],
+    details: {
+        job: "yes",
+        isMarrid: true,
+        status: "not found",
+        father: {
+            name:"hjsdbfs"
+        }
+    }
+}
+
+// console.log(person.1)
+
+console.log(person[true])
+
+
+// console.log(person["details"]["job"])
+
+
+// console.log(person["name"])
+
+// console.log(person.details?.mother?.name)
