3f3911d01d72e37575ac43c603be219676e4fc35
fatematujjohra <fatematujjohralima2@gmail.com>
2026-03-17 23:49:40 +0600

    mapping

---
+++ map.js
@@ -0,0 +1,42 @@
+
+
+
+// let temp = [];
+// for (let index = 0; index < numbers.length; index++) {
+//     const element = numbers[index];
+//     const sum = element + 1;
+//     temp.push(sum)
+    
+// }
+
+// console.log(temp)
+
+
+// const newArray = numbers.map(number => {
+//    return number+1
+// });
+
+// console.log(newArray)
+
+// const numbers = [1, 2, 3, 4, 5, 6];
+
+// const squareArray = numbers.map(element => {
+//     return element * element
+// });
+// console.log(squareArray)
+
+
+const friends = ["rahim", "korim", "jabbar", "hero alom"];
+
+const newFriends = friends.map((element, index) => {
+    console.log(index)
+    console.log(element)
+})
+
+// console.log(newFriends)
+
+// for (let i = 0; i < friends.length; i++) {
+//     const element = friends[i];
+//     console.log(element)
+    
+// }
